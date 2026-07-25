bf3757eab9dc7afa78d91731dbe135fa336faa81
jreans22 <james@roofqc.com>
2026-07-24 22:43:36 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
    SOLAR_KEY: Google Maps Platform key with the Solar API enabled.
    Must be referrer-restricted to *.roofqc.com/*. Paste the key between the quotes.
    Leave empty to disable satellite auto-measure (users just set roof size by slider). */
-window.SOLAR_KEY = "";
+window.SOLAR_KEY = "AIzaSyACM30aPH5bi8Rp_s0MBjJqpoELyc0QfHc";
